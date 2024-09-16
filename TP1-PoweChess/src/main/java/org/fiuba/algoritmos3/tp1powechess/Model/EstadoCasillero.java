87f6c734076a7da38e703a40b8e41f4d17b13b30
Javier Szwarcberg <jszwarcberg@fi.uba.ar>
2024-09-15 22:34:13 -0300

    Empiezo a crear EstadoCasillero

--- TP1-PoweChess/src/main/java/org/fiuba/algoritmos3/tp1powechess/Model/EstadoCasillero.java
+++ TP1-PoweChess/src/main/java/org/fiuba/algoritmos3/tp1powechess/Model/EstadoCasillero.java
@@ -2,4 +2,12 @@ package org.fiuba.algoritmos3.tp1powechess.Model;
 
 public interface EstadoCasillero {
     boolean estaOcupado();
+
+    default int getNumeroDeAmenazasBlanco() {
+        return 0;
+    }
+
+    default int getNumeroDeAmenazasNegro() {
+        return 0;
+    }
 }
